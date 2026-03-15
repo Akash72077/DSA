3d630080e9935e50f0bd667eaebf0e916b8fccbb
Akash72077 <sai.sai72077@gmail.com>
2026-03-15 14:57:35 +0530

    Decleared created and linked nodes

--- LinkedList/nthNodeFromEnd.java
+++ LinkedList/nthNodeFromEnd.java
@@ -1,4 +1,32 @@
 public class nthNodeFromEnd {
-    public static void main(String[] args) {    
+
+    public static class Node{  // defining nodes
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {  
+
+        //Creating a nodes
+            Node a= new Node(5);
+            Node b= new Node(10);
+            Node c= new Node(15);
+            Node d= new Node(20);
+            Node e= new Node(30);
+            Node f= new Node(34);
+            Node g= new Node(50);
+            Node h= new Node(90);
+            // linking Nodes 
+            a.next=b;
+            b.next=c;
+            c.next=d;
+            d.next=e;
+            e.next=f;
+            f.next=f;
+            g.next=h;
+            
+
     }
 }
